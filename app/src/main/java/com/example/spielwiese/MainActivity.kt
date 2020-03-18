43172923c132a909eb694ed06beb9762d42b199e
MarioEberth <marioeberth@gmail.com>
2020-03-18 17:20:18 +0100

    first commit of this project

---
+++ app/src/main/java/com/example/spielwiese/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.spielwiese
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myString = resources.getStringArray(R.array.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, myString
+            )
+            spinner.adapter = adapter
+        }
+    }
+}
